88a5155d8ffe52a25a6e30ba9125716ae8e2ee6a
Kartikey Kushwaha <73603370+kartikey1920@users.noreply.github.com>
2026-02-07 11:18:18 +0530

    Update page title and bear emojis in config.js

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Valentine? 💕",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💕'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        bears: ['🫰🏻', '🫶🏻']                       // Cute bear emojis
     },
 
     // Questions and answers
