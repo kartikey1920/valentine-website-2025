c26d4cf8606965ff07a36aa83e212229838490f3
Kartikey Kushwaha <73603370+kartikey1920@users.noreply.github.com>
2026-02-07 13:14:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️','🤍','💜','💙','💚','💛'],  // Heart emojis
+        hearts: ['❤️','🤍','❤️','🤍','❤️','🤍'],  // Heart emojis
         bears: ['🫰🏻', '🫶🏻','🌷','💌','💭']                       // Cute bear emojis
     },
 
